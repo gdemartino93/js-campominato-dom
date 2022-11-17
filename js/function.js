9bd6856161ed9a1ff115832cd33cb92ea9b3dbb4
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-11-17 19:43:40 +0100

    blocco pagina quando prendi la bomba

--- js/function.js
+++ js/function.js
@@ -2,7 +2,6 @@ function generaMinMax(min,max){
  return (Math.floor(Math.random() * ((max + 1) - min) + min))
 
 }
-
 function generaArrayNrRandom(quanti,numMin,numMax){
     const ARRAYNUMERI=[];
  while ((ARRAYNUMERI.length) < quanti){
--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ const CONTAINER = document.querySelector(".container");
 const SCELTA = document.getElementById("difficult");
 const ARRAYNUMERI=[]
 const NUMEROBOMBE= 16;
+const MAIN= document.querySelector("main")
+let score= 0;
 // dichiaro tasto play, container e la scelta della difficoltà
 
 // dichiaro tasto e container
@@ -22,40 +24,69 @@ BTNPLAY.addEventListener("click",
                 box.classList.add("boxstyle")
                 
                 box.addEventListener("click",
-                function(){
+                function(){                    
                     if (bombe.includes(i)){
-                        this.classList.toggle("bomba")
+                        this.classList.toggle("bomba");
+                        CONTAINER.innerHTML+=`<div class="velonero"></div>`;                      
+                    }else{
+                    this.classList.add("active");
+                    score++
+                    console.log(score);
                     }
-                    else{
-                        this.classList.toggle("active")
-                    }
-                    console.log(i)
-                    
                 })
-    
                 if (SCELTA.value == "ez"){
                     valueDifficult = 101;
                     box.classList.add("width-easy")
                 }
-                if (SCELTA.value == "normal"){
-                    valueDifficult = 82;
-                    box.classList.add("width-mid")
-                }
-                if (SCELTA.value == "hard"){
-                    valueDifficult = 50;
-                    box.classList.add("width-hard")
-                }
             }
         }
+        
         if (SCELTA.value == "normal"){
             valueDifficult = 82;
             let bombe= generaArrayNrRandom(NUMEROBOMBE,1,81)
             console.log(bombe);
+            for( let i = 1; i < valueDifficult; i++){
+                let box= document.createElement("div");
+                box.innerHTML = `${i}`
+                CONTAINER.append(box)
+                box.classList.add("boxstyle")
+                
+                box.addEventListener("click",
+                function(){
+                    if (bombe.includes(i)){
+                        this.classList.toggle("bomba")
+                    }
+                    console.log(i)
+                    this.classList.toggle("active")
+                })
+                if (SCELTA.value == "normal"){
+                    valueDifficult = 82;
+                    box.classList.add("width-mid")
+                }
+            }
         }
         if (SCELTA.value == "hard"){
             valueDifficult = 50;
             let bombe= generaArrayNrRandom(NUMEROBOMBE,1,49)
             console.log(bombe);
-        }
-        
+            for( let i = 1; i < valueDifficult; i++){
+                let box= document.createElement("div");
+                box.innerHTML = `${i}`
+                CONTAINER.append(box)
+                box.classList.add("boxstyle")
+                
+                box.addEventListener("click",
+                function(){
+                    if (bombe.includes(i)){
+                        box.classList.toggle("bomba")
+                    }
+                    console.log(i)
+                    this.classList.toggle("active")
+                })  
+                if (SCELTA.value == "hard"){
+                    valueDifficult = 50;
+                    box.classList.add("width-hard")
+                }
+            }
+        }        
 })
